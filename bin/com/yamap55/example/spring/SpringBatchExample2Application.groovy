ce4f16fb50a6c73facc9ce2633342d33911dfdba
yamap55 <tetuya120@hotmail.com>
2017-07-12 21:56:45 +0900

    [add] spring batch init

---
+++ bin/com/yamap55/example/spring/SpringBatchExample2Application.groovy
@@ -0,0 +1,12 @@
+package com.yamap55.example.spring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBatchExample2Application {
+
+	static void main(String[] args) {
+		SpringApplication.run SpringBatchExample2Application, args
+	}
+}
